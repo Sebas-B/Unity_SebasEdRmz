ff8cb0d5cb6153debebb03acab77c2e1521ae733
Sebas-B <sebedrmz1600@gmail.com>
2022-10-14 17:19:05 -0500

    reto 3

---
+++ unity3_unidad2/Reto3/Assets/Challenge 3/Scripts/MoveLeftX.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveLeftX : MonoBehaviour
+{
+    public float speed;
+    private PlayerControllerX playerControllerScript;
+    private float leftBound = -10;
+
+    void Start()
+    {
+        playerControllerScript = GameObject.Find("Player").GetComponent<PlayerControllerX>();
+    }
+
+    void Update()
+    {
+        // Si el juego no ha terminado se movera a la izquerda 
+        if (!playerControllerScript.gameOver)
+        {
+            transform.Translate(Vector3.left * speed * Time.deltaTime, Space.World);
+        }
+
+        // Si el objeto sale fuera de la pantalla y no es el fondo, se destruye
+        if (transform.position.x < leftBound && !gameObject.CompareTag("Background"))
+        {
+            Destroy(gameObject);
+        }
+
+    }
+}
---
+++ unity3_unidad2/Reto3/Assets/Challenge 3/Scripts/RepeatBackgroundX.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RepeatBackgroundX : MonoBehaviour
+{
+    private Vector3 startPos;
+    private float repeatWidth;
+
+    private void Start()
+    {
+        startPos = transform.position; // Estable la posicion default de inicio
+        repeatWidth = GetComponent<BoxCollider>().size.x / 2; // Marca la repeticion del ancho de la mitad del fondo
+    }
+
+    private void Update()
+    {
+        // If background moves left by its repeat width, move it back to start position
+        // Si el fondo se mueve a la izquierda se repita, de regraeso a la posicion de inicio
+        if (transform.position.x < startPos.x - repeatWidth)
+        {
+            transform.position = startPos;
+        }
+    }
+
+ 
+}
+
+
